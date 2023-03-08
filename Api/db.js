9fa34aceadc86de5120bc05be7bfec74eb68d74b
Vitin <127257016+VitorHugost@users.noreply.github.com>
2023-03-08 19:37:50 -0400

    Update db.js

--- Api/db.js
+++ Api/db.js
@@ -1,8 +1,10 @@
+//import da biblioteca mysql
 import mysql from "mysql";
 
+//Passando os parametros de conexao do meu Banco de dados
 export const db = mysql.createConnection({
-host: "localhost",
-user:"root",
-password:"Vitor99790691#",
-database:"americanas",
+  host: "localhost",
+  user:"root",
+  password:"Vitor99790691#",
+  database:"americanas",
 })
